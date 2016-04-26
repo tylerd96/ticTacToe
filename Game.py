967f53bb44fac5ba89bdffa6ff0499ff287b8d14
Tyler Davis <davistj7@MiamiOH.edu>
2016-04-26 11:22:14 -0400

    fixed data problem and got the selection screen to work

--- Game.py
+++ Game.py
@@ -35,25 +35,25 @@ def contents(S):
     """
 
     """
-    if GAME_START == False:
+    if S['START'] == False:
         A = [('Press to start the game',240,460,20),(200,440,280,410,YELLOW),\
              ('START', 240, 425, 15), ('Select the game mode:', 440, 345,20)]
-        if GAME_MODE == 0:
+        if S['MODE'] == 0:
             return A + [(380,315,500,285,GREEN),('Two Player',440,300,13),\
                      (380,275,500,245,RED), ('VS. Easy AI', 440,260,13),\
                      (380,235,500,205,RED), ('VS. Medium AI', 440,220,13),\
                      (380,195,500,165,RED), ('VS. Hard AI', 440,180,13)]
-        if GAME_MODE == 1:
+        if S['MODE'] == 1:
             return A + [(380,315,500,285,RED),('Two Player',440,300,13),\
                      (380,275,500,245,GREEN), ('VS. Easy AI', 440,260,13),\
                      (380,235,500,205,RED), ('VS. Medium AI', 440,220,13),\
                      (380,195,500,165,RED), ('VS. Hard AI', 440,180,13)]
-        if GAME_MODE == 2:
+        if S['MODE'] == 2:
             return A + [(380,315,500,285,RED),('Two Player',440,300,13),\
                      (380,275,500,245,RED), ('VS. Easy AI', 440,260,13),\
                      (380,235,500,205,GREEN), ('VS. Medium AI', 440,220,13),\
                      (380,195,500,165,RED), ('VS. Hard AI', 440,180,13)]
-        if GAME_MODE == 3:
+        if S['MODE'] == 3:
             return A + [(380,315,500,285,RED),('Two Player',440,300,13),\
                      (380,275,500,245,RED), ('VS. Easy AI', 440,260,13),\
                      (380,235,500,205,RED), ('VS. Medium AI', 440,220,13),\
@@ -64,19 +64,20 @@ def contents(S):
         return []
 
 def successor_state(S,P):
-    if(not GAME_START):
+    if(not S['START']):
        if(in_2Player(P)):
-           GAME_MODE = 0
+           S['MODE'] = 0
        if(in_Easy(P)):
-            GAME_MODE = 1
+            S['MODE'] = 1
        if(in_Med(P)):
-            GAME_MODE = 2
+            S['MODE'] = 2
        if(in_Hard(P)):
-            GAME_MODE = 3
-       print(S)
+            S['MODE'] = 3
+       if(in_Start(P)):
+           S['START'] = True
     else:
         print('hello')
-    return (S,P)
+    return S
 
 def game_over(S):
     return has_won('x',S) or has_won('o',S)
@@ -114,6 +115,8 @@ def in_Med(P):
 def in_Hard(P):
     (x,y) = P
     return 380 <= x <= 500 and 165 <= y <= 195
-
+def in_Start(P):
+    (x,y) = P
+    return 200 <= x <= 280 and 410 <= y <= 440
 
 run_game(game_title, initial_state, successor_state, game_over, images)
